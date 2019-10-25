563c7159ad6591d448878aa257f4dc34f10cadc9
Sonqo <strats_karyp@hotmail.com>
2019-10-25 11:08:25 +0300

    Description commments optimization

--- 2_Micro_Computers_Lab/Exercise_2/Ex_3.asm
+++ 2_Micro_Computers_Lab/Exercise_2/Ex_3.asm
@@ -4,7 +4,7 @@
 	rjmp reset
 .org 0x4					; INT1 address
 	rjmp isr1
-.org 0x10
+.org 0x10					; TIMER1 address
 	rjmp timer1
 
 reset:
@@ -18,7 +18,7 @@ reset:
 	ldi r24, (1<<INT1)		; enabling INT1 interrupt
 	out GICR, r24
 
-	clr r21					; initialization of flag = 0
+	clr r21					; initialization of binary flag(0,1)
 	clr r24
 	out DDRA, r24			; initialization PORTA for input
 	ser r24	
@@ -26,7 +26,7 @@ reset:
 	
 	ldi r24, (1<<TOIE1)		; initialization of TCNT1 interrupt for TIMER1
 	out TIMSK, r24
-	ldi r24, (1<<CS12)|(0<<CS11)|(1<<CS10)		;1/1024
+	ldi r24, (1<<CS12)|(0<<CS11)|(1<<CS10)		;1/1024 for 8MHz AVR frequency
 	out TCCR1B, r24
 	sei						; enable global interrupts
 
@@ -36,15 +36,15 @@ main:
 	cpi r23, 0x80
 	brne main
 	
-	ldi r23, 0x85			; initialize TIMER1 for 4 seconds interrupt
+	ldi r23, 0x85			; else initialize TIMER1 for 4 seconds interrupt
 	out TCNT1H, r23			
 	ldi r23, 0xEE
 	out TCNT1L, r23
 	
-	cpi r21, 0x00			; check if LEDs are already open
+	cpi r21, 0x00			; check flag in case LEDs are already open
 	breq first_time
 
-	ser r23					; if LSB(B) is 1, open all the other LEDs as well
+	ser r23					; if flag is 1, open all the other LEDs as well
 	out PORTB, r23
 	ldi r24, low(500)		; 1/2 a second delay for all LEDs
 	ldi r25, high(500)
@@ -78,16 +78,16 @@ isr1:
 	cpi r21, 0x00			; check if LEDs are already open
 	breq first_time_int
 
-	ser r23					; if LSB(B) is 1, open all the other LEDs as well
+	ser r23					; else open all the other LEDs as well
 	out PORTB, r23
-	ldi r24, low(500)
+	ldi r24, low(500)		; 1/2 a second delay
 	ldi r25, high(500)
 	rcall wait_msec
 	
 first_time_int:
 	ldi r21, 0x01			; set flag to 1
 	ldi r23, 0x01
-	out PORTB, r23			 
+	out PORTB, r23			; turn just the MSB(B) on
 
 	pop r23
 	out SREG, r23			; retrieving SREG from stack
@@ -95,26 +95,17 @@ first_time_int:
 	ldi r25, high(500)
 	reti
 
-timer1:
+timer1:						; TIMER1 rotuine
 	in r20, SREG			; save SREG to stack
 	push r20
-	ldi r21, 0x00			; set flag back to 0, since all LED's will turn off
+	ldi r21, 0x00			; set flag back to 0, since all LED's will be turned off
 	clr r23
 	out PORTB, r23
 	pop r20					; retrieve SREG from stack
 	out SREG, r20
 	reti
 
-wait_usec:
-	sbiw r24, 1 
-	nop 
-	nop 
-	nop 
-	nop 
-	brne wait_usec 
-	ret 
-
-wait_msec:
+wait_msec:					; interrupt routine, calls wait_usec
 	push r24 
 	push r25 
 	ldi r24, low(998) 
@@ -124,4 +115,13 @@ wait_msec:
 	pop r24 
 	sbiw r24, 1 
 	brne wait_msec 
+	ret 
+
+wait_usec:
+	sbiw r24, 1 
+	nop 
+	nop 
+	nop 
+	nop 
+	brne wait_usec 
 	ret 
