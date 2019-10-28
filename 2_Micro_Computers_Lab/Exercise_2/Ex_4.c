b5869719bbfa26338d180d1aa0e93ad02efaa1dc
Sonqo <strats_karyp@hotmail.com>
2019-10-28 14:39:11 +0200

    Completed ISR1 routine and improved TIMER1 routine

--- 2_Micro_Computers_Lab/Exercise_2/Ex_4.c
+++ 2_Micro_Computers_Lab/Exercise_2/Ex_4.c
@@ -1,58 +1,73 @@
 #include <avr/io.h>
 #include <avr/interrupt.h>
 
-int volatile x, flag;
+int volatile flag;
 
-ISR(TIMER1_OVF_vect){	// TIMER1 ISR
+void initInterrupt(){
+	
+	GICR = (1<<INT1);
+	MCUCR = (1<<ISC11) | (1<<ISC10);
+}
+
+void initTIMER1(){
+	
+	TCCR1B = (1<<CS10) | (1<<CS12);
+	TIMSK = (1 << TOIE1) ;
+}
+
+ISR(TIMER1_OVF_vect){ // TIMER1 ISR
 	
 	if (flag == 1){
 		flag--;
-		x = 0x00;
-		PORTB = x;
+		PORTB = 0x00;
 	}
 	else if (flag == 2){
 		flag = 0;
-		x = 0x01;
-		PORTB = x;
+		PORTB = 0x01;
 		TCNT1 = 0x85EE;
 	}
 }
 
 ISR(INT1_vect){
 	
+	if (flag == 0){
+		flag++;
+		PORTB = 0x01;
+		TCNT1 = 0x85EE; // 4 seconds delay
+	}
+	else{
+		flag++;
+		PORTB = 0xFF;
+		TCNT1 = 0xF0BD; // 1/2 a second delay
+	}
 }
 
 int main(void){
 	
-	DDRA = 0X00;		// initializing PORTD as input
-	DDRB = 0XFF;		// initializing PORTB as output
+	DDRA = 0X00; // initializing PORTA as input
+	DDRB = 0XFF; // initializing PORTB as output
 	
-	x = 0;				// initialization of LED variable
-	flag = 0;			// initialization of flag
+	flag = 0; // initialization of flag
 	
-	TCCR1B = (1<<CS10) | (1<<CS12);			// 1/1024 prescaler
-	TIMSK = (1 << TOIE1) ; 
-	
-	sei();
+	initInterrupt();
+	initTIMER1();	
+	sei(); // enabling global interrupts
 	
     while (1) {
 		
-		if ((PINA & 0X01) == 1){
+		if (PA7 == 1){
 			
 			if (flag == 0){
-				x = 0x01;
 				flag++;
-				PORTB = x;
-				TCNT1 = 0x85EE;				// 4 seconds delay
+				PORTB = 0x01;
+				TCNT1 = 0x85EE; // 4 seconds delay
 			}
 			else{
 				flag++;
-				x = 0xFF;
-				PORTB = x;
-				TCNT1 = 0xF0BD;				// 1/2 a second delay
+				PORTB = 0xFF;
+				TCNT1 = 0xF0BD; // 1/2 a second delay
 			}
 		}
     }
 	return 0;
 }
-
