631fd89c73e37b4bcd35427e7501e3dcba22e87e
Sonqo <strats_karyp@hotmail.com>
2019-11-08 21:18:31 +0200

    Added keypad-reading functions

---
+++ 2_Micro_Computers_Lab/Exercise_3/Ex_1.asm
@@ -0,0 +1,50 @@
+.DSEG
+	_tmp_: .byte 2
+
+.CSEG
+		ldi r24, (1<<PC7)|(1<<PC6)|(1<<PC5)|(1<<PC4)		; initializing PORTC for keypad scanning
+		out DDR4, r24
+
+scan_row:						; scanning row number r24 for pressed keys, returns respective status in r24
+		ldi r25, 0x08
+back:	lsl r25
+		dec r24
+		brne back
+		out PORTC, r25
+		nop						; waitng for status switching to take place
+		nop
+		in r24, PINC
+		andi r24, 0x0F			; status isolated in LSBs of r24
+		ret
+
+scan_keypad:					; returns in register pair r25:r24 the keypad status of all 16 keys - calls scan row
+		ldi r24, 0x01
+		rcall scan_now			; checking 1st line
+		swap r24
+		mov r27, r24
+		ldi r24, 0x02
+		rcall scan_now			; checking 2nd line
+		add r27, r24
+		ldi r24, 0x03
+		rcall scan_now			; checking 3rd line
+		swap r24
+		mov r26, r24
+		ldi r24, 0x04
+		rcall scan_now			; checking 4th line
+		add r26, r24
+		movw r24, r26
+		ret
+
+scan_keypad_rising_edge:		
+		mov r22, r24			; save delay timer(r24) before first read
+		rcall scan_keypad
+		push r24				; save status to stack
+		push r25
+		mov r24, r22	
+		ldi r25, 0x00
+		rcall wait_msec			; delay before rechecking
+		rcall scan_keypad
+		pop r23
+		pop r22
+		and r24, r22			; discard
+		and r25, r23
