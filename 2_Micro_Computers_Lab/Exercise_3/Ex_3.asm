676e3c4c00aa31b508a8b5a148f296e2f315844d
Sonqo <strats_karyp@hotmail.com>
2019-11-20 22:06:04 +0200

    TODO: complete main routine, add hex_to_ascii routine

--- 2_Micro_Computers_Lab/Exercise_3/Ex_3.asm
+++ 2_Micro_Computers_Lab/Exercise_3/Ex_3.asm
@@ -12,9 +12,56 @@ main:
 	out SPH, r24			
 	ser r24					; initializing PORTD for output
 	out DDRD, r24
+	
 	clr r24
 	rcall lcd_init
 
+start:
+	clr r24					; initialize _tmp_ = 0
+	ldi XH, high(_tmp_)
+	ldi XL, low(_tmp_)
+	st X+,r24
+	st X,r24
+
+scan_first:
+	ldi r24, low(20)
+	rcall scan_keypad_rising_edge
+	rcall keypad_to_hex
+	cpi r24, 0xFF
+	breq scan_first
+	;rcall keypad_to_hex
+	push r24
+
+scan_second:
+	ldi r24, low(20)
+	rcall scan_keypad_rising_edge
+	rcall keypad_to_hex
+	cpi r24, 0xFF
+	breq scan_second
+	;rcall keypad_to_hex
+	pop r25
+
+	mov r26, r24
+	mov r24, r25
+	rcall keypad_to_ascii
+	rcall lcd_data
+	mov r24, r26
+	rcall keypad_to_ascii
+	rcall lcd_data
+	ldi r24, 0x02
+	rcall lcd_command
+	rjmp start
+
+; NEW ROUTINE - IN PROGRESS ;
+	add r24, r25
+	ror r24
+	brcc minus
+	ldi r17, '+'
+	rjmp digits
+minus:
+	ldi r17, '-'
+digits:
+	
 write_2_nibbles:
 	push r24
 	in r25, PIND 
@@ -88,112 +135,166 @@ lcd_init:
 	ret
 
 scan_row:				; scanning row number r24 for pressed keys, returns respective status in r24
-		ldi r25, 0x08
-back:	lsl r25
-		dec r24
-		brne back
-		out PORTC, r25
-		nop				; waitng for status switching to take place
-		nop
-		in r24, PINC
-		andi r24, 0x0F	; status isolated in LSBs of r24
-		ret
+	ldi r25, 0x08
+back_:	
+	lsl r25
+	dec r24
+	brne back_
+	out PORTC, r25
+	nop				; waitng for status switching to take place
+	nop
+	in r24, PINC
+	andi r24, 0x0F	; status isolated in LSBs of r24
+	ret
 
 scan_keypad:			; returns in register pair r25:r24 the keypad status of all 16 keys - calls scan row
-		ldi r24, 0x01
-		rcall scan_row	; checking 1st line
-		swap r24
-		mov r27, r24
-		ldi r24, 0x02
-		rcall scan_row	; checking 2nd line
-		add r27, r24
-		ldi r24, 0x03
-		rcall scan_row	; checking 3rd line
-		swap r24
-		mov r26, r24
-		ldi r24, 0x04
-		rcall scan_row	; checking 4th line
-		add r26, r24
-		movw r24, r26
-		ret
+	ldi r24, 0x01
+	rcall scan_row	; checking 1st line
+	swap r24
+	mov r27, r24
+	ldi r24, 0x02
+	rcall scan_row	; checking 2nd line
+	add r27, r24
+	ldi r24, 0x03
+	rcall scan_row	; checking 3rd line
+	swap r24
+	mov r26, r24
+	ldi r24, 0x04
+	rcall scan_row	; checking 4th line
+	add r26, r24
+	movw r24, r26
+	ret
 
 scan_keypad_rising_edge:	
-		mov r22, r24	; save delay timer(r24) before first read
-		rcall scan_keypad
-		push r24		; save status to stack
-		push r25
-		mov r24, r22	
-		ldi r25, 0x00
-		rcall wait_msec	; delay before rechecking
-		rcall scan_keypad
-		pop r23
-		pop r22
-		and r24, r22	; discard
-		and r25, r23
-		ldi r26, low(_tmp_)
-		ldi r27, high(_tmp_)
-		ld r23, X+
-		ld r22, X
-		st X, r24 
-		st -X, r25 
-		com r23
-		com r22 
-		and r24, r22
-		and r25, r23
-		ret
+	mov r22, r24	; save delay timer(r24) before first read
+	rcall scan_keypad
+	push r24		; save status to stack
+	push r25
+	mov r24, r22	
+	ldi r25, 0x00
+	rcall wait_msec	; delay before rechecking
+	rcall scan_keypad
+	pop r23
+	pop r22
+	and r24, r22	; discard
+	and r25, r23
+	ldi r26, low(_tmp_)
+	ldi r27, high(_tmp_)
+	ld r23, X+
+	ld r22, X
+	st X, r24 
+	st -X, r25 
+	com r23
+	com r22 
+	and r24, r22
+	and r25, r23
+	ret
+
+keypad_to_ascii: 
+	movw r26, r24 
+	ldi r24, '*'
+	sbrc r26, 0
+	ret
+	ldi r24, '0'
+	sbrc r26, 1
+	ret
+	ldi r24, '#'
+	sbrc r26, 2
+	ret
+	ldi r24, 'D'
+	sbrc r26, 3 
+	ret 
+	ldi r24, '7'
+	sbrc r26, 4
+	ret
+	ldi r24, '8'
+	sbrc r26, 5
+	ret
+	ldi r24, '9'
+	sbrc r26, 6
+	ret
+	ldi r24, 'C'
+	sbrc r26, 7
+	ret
+	ldi r24, '4'
+	sbrc r27, 0 
+	ret
+	ldi r24, '5'
+	sbrc r27, 1
+	ret
+	ldi r24, '6'
+	sbrc r27, 2
+	ret
+	ldi r24, 'B'
+	sbrc r27, 3
+	ret
+	ldi r24, '1'
+	sbrc r27, 4
+	ret
+	ldi r24, '2'
+	sbrc r27, 5
+	ret
+	ldi r24, '3'
+	sbrc r27, 6
+	ret
+	ldi r24, 'A'
+	sbrc r27, 7
+	ret
+	clr r24
+	ret
 
 keypad_to_hex: 
-		movw r26, r24 
-		ldi r24, 0x0E
-		sbrc r26, 0
-		ret
-		ldi r24, 0x00
-		sbrc r26, 1
-		ret
-		ldi r24, 0x0F
-		sbrc r26, 2
-		ret
-		ldi r24, 0x0D
-		sbrc r26, 3 
-		ret 
-		ldi r24, 0x07
-		sbrc r26, 4
-		ret
-		ldi r24, 0x08
-		sbrc r26, 5
-		ret
-		ldi r24, 0x09
-		sbrc r26, 6
-		ret
-		ldi r24, 0x0C
-		sbrc r26, 7
-		ret
-		ldi r24, 0x04
-		sbrc r27, 0 
-		ret
-		ldi r24, 0x05
-		sbrc r27, 1
-		ret
-		ldi r24, 0x06
-		sbrc r27, 2
-		ret
-		ldi r24, 0x0B
-		sbrc r27, 3
-		ret
-		ldi r24, 0x01
-		sbrc r27, 4
-		ret
-		ldi r24, 0x02
-		sbrc r27, 5
-		ret
-		ldi r24, 0x03
-		sbrc r27, 6
-		ret
-		ldi r24, 0x0A
-		sbrc r27, 7
-		ret
-		clr r24
-		ret
+	movw r26, r24 
+	ldi r24, 0x0E
+	sbrc r26, 0
+	ret
+	ldi r24, 0x00
+	sbrc r26, 1
+	ret
+	ldi r24, 0x0F
+	sbrc r26, 2
+	ret
+	ldi r24, 0x0D
+	sbrc r26, 3 
+	ret 
+	ldi r24, 0x07
+	sbrc r26, 4
+	ret
+	ldi r24, 0x08
+	sbrc r26, 5
+	ret
+	ldi r24, 0x09
+	sbrc r26, 6
+	ret
+	ldi r24, 0x0C
+	sbrc r26, 7
+	ret
+	ldi r24, 0x04
+	sbrc r27, 0 
+	ret
+	ldi r24, 0x05
+	sbrc r27, 1
+	ret
+	ldi r24, 0x06
+	sbrc r27, 2
+	ret
+	ldi r24, 0x0B
+	sbrc r27, 3
+	ret
+	ldi r24, 0x01
+	sbrc r27, 4
+	ret
+	ldi r24, 0x02
+	sbrc r27, 5
+	ret
+	ldi r24, 0x03
+	sbrc r27, 6
+	ret
+	ldi r24, 0x0A
+	sbrc r27, 7
+	ret
+	ldi r24, 0xFF	; error handling
+	ret
 
 wait_msec:			; delay routine, calls wait_usec
 	push r24 
