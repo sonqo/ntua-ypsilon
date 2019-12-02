45add0391d8a9ee58bae9f2bbfd53c020e4c37e8
Sonqo <strats_karyp@hotmail.com>
2019-12-02 23:19:11 +0200

    Completed all DS1820 relative routines

--- 2_Micro_Computers_Lab/Exercise_4/Ex_2.c
+++ 2_Micro_Computers_Lab/Exercise_4/Ex_2.c
@@ -23,13 +23,109 @@ char one_wire_reset(void){
 	}
 }
 
+void one_wire_transmit_bit(char bit){
+	
+	bit   &= 0b00000001; // clear all but the LSB
+	
+	DDRA |= (1 << PA4); // PA4 configured for output
+	PORTA &= ~(1 << PA4); // set PA4 to 0
+	_delay_us(2);
+	if (bit == 1){
+		PORTA |= (1 << PA4);
+	}
+	else{
+		PORTA &= ~(1 << PA4);
+	}
+	_delay_us(58);
+	DDRA  &= ~(1 << PA4);
+	PORTA &= ~(1 << PA4);
+	_delay_us(1);
+	return;
+}
 
+void one_wire_transmit_byte(char byte){
+	for (int i=0; i<8; i++){
+		if ((byte & 0x01) == 0x01){
+			one_wire_transmit_bit(1);
+		}
+		else{
+			one_wire_transmit_bit(0);
+		}
+		byte >>= 1; // right shift one place
+	}
+}
+
+char one_wire_receive_bit(void){
+	
+	short bit;
+	
+	DDRA |= (1 << PA4);
+	PORTA &= ~(1 << PA4);
+	_delay_us(2);
+	DDRA  &= ~(1 << PA4);
+	PORTA &= ~(1 << PA4);
+	_delay_us(10);
+	if ((PINA & 0x10 ) == 0x10){
+		bit = 1;
+	}
+	else{
+		bit =0;
+	}
+	_delay_us(49);
+	return bit;
+}
+
+char one_wire_receive_byte(void){
+	
+	char bit;
+	char byte = 0x00;
+	
+	for (int i=0; i<8; i++){
+		bit = one_wire_receive_bit();
+		byte >>= 1;
+		if (bit == 1){
+			byte |= 0x80;
+		}
+	}
+	return byte;
+}
+
+int ds1820_routine(void) {
+	char MSB,LSB;
+	int result = 0;
+	char control = 0;
+	control = one_wire_reset();
+	if (control == 0x01){
+		one_wire_transmit_byte(0xCC);
+		one_wire_transmit_byte(0x44);
+		while (!one_wire_receive_bit()) ;
+		control = one_wire_reset();
+		if (control == 0x01) {
+			one_wire_transmit_byte(0xCC);
+			one_wire_transmit_byte(0xBE);
+			LSB = one_wire_receive_byte();
+			MSB = one_wire_receive_byte();
+			result = (MSB << 8) + LSB;
+			result >>= 1;
+			return result;
+		}
+		else{ 
+			return 0x8000;
+		}
+	}
+	else{ 
+		return 0x8000;
+	}
+}
 
 int main(void)
 {
-    /* Replace with your application code */
-    while (1) 
-    {
+	DDRB = 0xFF;
+	char temperature;
+	
+    while (1){
+		temperature = ds1820_routine();
+		PORTB = temperature;
     }
 }
 
