7a5d91edc74e0e32a3a41770d27f8c3977ea6881
Sonqo <strats_karyp@hotmail.com>
2020-03-12 17:53:11 +0200

    Corrections on encryption-decryption

--- 4_Operating_Systems/Exercise_1/Caesar_Encryption.c
+++ 4_Operating_Systems/Exercise_1/Caesar_Encryption.c
@@ -15,7 +15,8 @@
 
 typedef enum { ENCRYPT, DECRYPT } encrypt_mode;
 
-char caesarChar(unsigned char ch, encrypt_mode mode, int key) { // Encrypting characters
+char caesarChar(unsigned char ch, encrypt_mode mode, int key) {
+    // Encrypting characters according to key
     if (ch >= 'a' && ch <= 'z') {
         if (mode == ENCRYPT) {
             ch += key;
@@ -39,20 +40,21 @@ char caesarChar(unsigned char ch, encrypt_mode mode, int key) { // Encrypting ch
     return ch;
 }
 
-char* caesarString(char* string, encrypt_mode mode, int key, char* result) { // Encrypting strings
-    for (int i = 0; i <= strlen(string); i++){
+char* caesarString(char* string, encrypt_mode mode, int key, char* result) {
+    // Encrypting strings according to key, calls caesarChar
+    for (int i = 0; i <= strlen(string); i++) {
         result[i] = caesarChar(string[i], mode, key);
     }
     return result;
 }
 
-void errorRW(){
+void errorRW() {
     printf("Reading or writing failed\n");
     exit(EXIT_FAILURE);
 }
 
-void noFile(){
-    printf("Input or output files not found\n");
+void noFile() {
+    printf("Input file could not be found\n");
     exit(EXIT_FAILURE);
 }
 
@@ -61,16 +63,18 @@ void correctUsage() {
     exit(EXIT_FAILURE);
 }
 
+// Starting point
 int main(int argc, char **argv) {
 
-    int fp1, fp2;
+    int f = 0;
+    int fp1, fp2; // file pointers
     int n_read, n_write;
-
-    char buff[BUFFER_SIZE];
-    char result[BUFFER_SIZE];
 
     int chosen_key = -1;
     const char *chosen_file = NULL;
+
+    char buff[BUFFER_SIZE];
+    char result[BUFFER_SIZE];
 
     // Parse command-line arguments
     for (int i = 1; i < argc; i++) {
@@ -95,44 +99,54 @@ int main(int argc, char **argv) {
             else { // argv[i] == "--file", so argv[i+1] has the file name
                 int is_valid_file = VALIDFILE(argv[i+1]);
                 if (is_valid_file) { // file name not blank
+                    f = 1;
                     chosen_file = argv[i + 1];
-                }
-                else {
-                    correctUsage();
                 }
             }
         }
     }
-
-    if (!chosen_file || chosen_key == -1) {
+    
+    // Command handling
+    if (chosen_key == -1) {
         correctUsage();
     }
     else {
         pid_t first_pid = fork(); // parent initializes C1
-        if (first_pid == 0){
-            fp1 = open(chosen_file, O_RDONLY);
-            fp2 = open("./encrypted.txt", O_WRONLY);
-            if (fp1 == -1  || fp2 == -1){ // error opening the specified files
-                noFile();
+        if (first_pid == 0) { // C1 functionality
+            if (f == 1) {
+                fp1 = open(chosen_file, O_RDONLY);
+                fp2 = open("./encrypted.txt", O_RDWR | O_TRUNC | O_CREAT, 0777);
+                if (fp1 == -1){ // error opening the specified files
+                    noFile();
+                }
+                n_read = read(fp1, buff, BUFFER_SIZE);
+                n_write = write(fp2, caesarString(buff, ENCRYPT, chosen_key, result), n_read);
+                if (n_write < n_read) { // reading failed, more characters read than written
+                    errorRW();
+                }
+                close(fp1);
+                close(fp2);
             }
-            n_read = read(fp1, buff, BUFFER_SIZE);
-            n_write = write(fp2, caesarString(buff, ENCRYPT, chosen_key, result), n_read);
-            if (n_read == -1 || n_write < n_read){ // more characters written than read
-                errorRW();
+            else if (f == 0) {
+                fp1 = open("./encrypted.txt", O_WRONLY | O_TRUNC | O_CREAT, 0777);
+                n_read = read(0, buff, BUFFER_SIZE); // read from STD_INPUT
+                n_write = write(fp1, caesarString(buff, ENCRYPT, chosen_key, result), n_read);
+                if (n_write < n_read) { // reading failed, more characters read than written
+                    errorRW();
+                }
+                close(fp1);
             }
-            close(fp1);
-            close(fp2);
         }
         else {
             wait(NULL); // wait C1 completion
         }
         if (first_pid > 0) {
             pid_t second_pid = fork(); // parent initializes C2
-            if (second_pid == 0) {
+            if (second_pid == 0) { // C2 functionality
                 fp1 = open("./encrypted.txt", O_RDONLY);
                 n_read = read(fp1, buff, BUFFER_SIZE);
-                n_write = write(1, caesarString(buff, DECRYPT, chosen_key, result), n_read);
-                if (n_read == -1 || n_write < n_read){ // more characters written than read
+                n_write = write(1, caesarString(buff, DECRYPT, chosen_key, result), n_read); // write to STD_OUTPUT
+                if (n_write < n_read) { // reading failed, more characters read than written
                     errorRW();
                 }
                 close(fp1);
