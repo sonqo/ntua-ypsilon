56d31d2fe3bf35b26172e7db0181c09c38a9ea22
Sonqo <strats_karyp@hotmail.com>
2020-08-09 12:15:26 +0300

    Completed initialization of stations

--- Airport/src/app/gui/Controller.java
+++ Airport/src/app/gui/Controller.java
@@ -9,8 +9,10 @@ import java.io.File;
 import java.net.URL;
 import java.util.List;
 import java.util.Arrays;
+import javafx.fxml.FXML;
 import javafx.stage.Stage;
 import java.io.FileReader;
+import java.util.ArrayList;
 import java.io.IOException;
 import javafx.util.Duration;
 import java.io.BufferedReader;
@@ -24,11 +26,35 @@ import javafx.animation.Animation;
 import javafx.scene.control.Label;
 import java.net.URISyntaxException;
 import javafx.scene.control.MenuBar;
+import javafx.scene.layout.AnchorPane;
 
 public class Controller implements Initializable {
 
     public MenuBar menuBar;
-    public Label timerLabel, gateNum, tradeNum, zoneANum, zoneBNum, zoneCNum, generalNum, longNum;
+    public Label timerLabel;
+
+    @FXML
+    private List<AnchorPane> globalGateAnchorList;
+    @FXML
+    private List<AnchorPane> globalTradeAnchorList;
+    @FXML
+    private List<AnchorPane> globalZoneAAnchorList;
+    @FXML
+    private List<AnchorPane> globalZoneBAnchorList;
+    @FXML
+    private List<AnchorPane> globalZoneCAnchorList;
+    @FXML
+    private List<AnchorPane> globalGeneralAnchorList;
+    @FXML
+    private List<AnchorPane> globalLongTermAnchorList;
+
+    private final List<AnchorPane> validGateAnchorList = new ArrayList<>();
+    private final List<AnchorPane> validTradeAnchorList = new ArrayList<>();
+    private final List<AnchorPane> validZoneAAnchorList = new ArrayList<>();
+    private final List<AnchorPane> validZoneBAnchorList = new ArrayList<>();
+    private final List<AnchorPane> validZoneCAnchorList = new ArrayList<>();
+    private final List<AnchorPane> validGeneralAnchorList = new ArrayList<>();
+    private final List<AnchorPane> validLongTermAnchorList = new ArrayList<>();
 
     int hours = 0;
     int minutes = 0;
@@ -52,37 +78,86 @@ public class Controller implements Initializable {
             if (Integer.parseInt(rtmp.get(0)) == 1) {
                 GateStation stat = new GateStation();
                 stat.setInfo(Integer.parseInt(rtmp.get(0)), Integer.parseInt(rtmp.get(1)), Integer.parseInt(rtmp.get(2)), rtmp.get(3));
-                airport.station_list.add(stat);
+                airport.gateStationList.add(stat);
+                for (int i=0; i<airport.gateStationList.get(0).spot_num; i++){
+                    validGateAnchorList.add(globalGateAnchorList.remove(0));
+                }
+                airport.gateAnchorList = validGateAnchorList;
+                for (int i=0; i<airport.gateAnchorList.size(); i++){
+                    airport.gateAnchorList.get(i).setStyle("-fx-background-color: palevioletred; -fx-border-color: white");
+                }
             }
             else if (Integer.parseInt(rtmp.get(0)) == 2){
                 TradeStation stat = new TradeStation();
                 stat.setInfo(Integer.parseInt(rtmp.get(0)), Integer.parseInt(rtmp.get(1)), Integer.parseInt(rtmp.get(2)), rtmp.get(3));
-                airport.station_list.add(stat);
+                airport.tradeStationList.add(stat);
+                for (int i=0; i<airport.tradeStationList.get(0).spot_num; i++){
+                    validTradeAnchorList.add(globalTradeAnchorList.remove(0));
+                }
+                airport.tradeAnchorList = validTradeAnchorList;
+                for (int i=0; i<airport.tradeAnchorList.size(); i++){
+                    airport.tradeAnchorList.get(i).setStyle("-fx-background-color: palevioletred; -fx-border-color: white");
+                }
             }
             else if (Integer.parseInt(rtmp.get(0)) == 3){
                 ZoneAStation stat = new ZoneAStation();
                 stat.setInfo(Integer.parseInt(rtmp.get(0)), Integer.parseInt(rtmp.get(1)), Integer.parseInt(rtmp.get(2)), rtmp.get(3));
-                airport.station_list.add(stat);
+                airport.zoneAStationList.add(stat);
+                for (int i=0; i<airport.zoneAStationList.get(0).spot_num; i++){
+                    validZoneAAnchorList.add(globalZoneAAnchorList.remove(0));
+                }
+                airport.zoneAAnchorList = validZoneAAnchorList;
+                for (int i=0; i<airport.zoneAAnchorList.size(); i++){
+                    airport.zoneAAnchorList.get(i).setStyle("-fx-background-color: palevioletred; -fx-border-color: white");
+                }
             }
             else if (Integer.parseInt(rtmp.get(0)) == 4){
                 ZoneBStation stat = new ZoneBStation();
                 stat.setInfo(Integer.parseInt(rtmp.get(0)), Integer.parseInt(rtmp.get(1)), Integer.parseInt(rtmp.get(2)), rtmp.get(3));
-                airport.station_list.add(stat);
+                airport.zoneBStationList.add(stat);
+                for (int i=0; i<airport.zoneBStationList.get(0).spot_num; i++){
+                    validZoneBAnchorList.add(globalZoneBAnchorList.remove(0));
+                }
+                airport.zoneBAnchorList = validZoneBAnchorList;
+                for (int i=0; i<airport.zoneBAnchorList.size(); i++){
+                    airport.zoneBAnchorList.get(i).setStyle("-fx-background-color: palevioletred; -fx-border-color: white");
+                }
             }
             else if (Integer.parseInt(rtmp.get(0)) == 5){
                 ZoneCStation stat = new ZoneCStation();
                 stat.setInfo(Integer.parseInt(rtmp.get(0)), Integer.parseInt(rtmp.get(1)), Integer.parseInt(rtmp.get(2)), rtmp.get(3));
-                airport.station_list.add(stat);
+                airport.zoneCStationList.add(stat);
+                for (int i=0; i<airport.zoneCStationList.get(0).spot_num; i++){
+                    validZoneCAnchorList.add(globalZoneCAnchorList.remove(0));
+                }
+                airport.zoneCAnchorList = validZoneCAnchorList;
+                for (int i=0; i<airport.zoneCAnchorList.size(); i++){
+                    airport.zoneCAnchorList.get(i).setStyle("-fx-background-color: palevioletred; -fx-border-color: white");
+                }
             }
             else if (Integer.parseInt(rtmp.get(0)) == 6){
                 GeneralStation stat = new GeneralStation();
                 stat.setInfo(Integer.parseInt(rtmp.get(0)), Integer.parseInt(rtmp.get(1)), Integer.parseInt(rtmp.get(2)), rtmp.get(3));
-                airport.station_list.add(stat);
+                airport.generalStationList.add(stat);
+                for (int i=0; i<airport.generalStationList.get(0).spot_num; i++){
+                    validGeneralAnchorList.add(globalGeneralAnchorList.remove(0));
+                }
+                airport.generalAnchorList = validGeneralAnchorList;
+                for (int i=0; i<airport.generalAnchorList.size(); i++){
+                    airport.generalAnchorList.get(i).setStyle("-fx-background-color: palevioletred; -fx-border-color: white");
+                }
             }
             else if (Integer.parseInt(rtmp.get(0)) == 7){
                 LongTermStation stat = new LongTermStation();
                 stat.setInfo(Integer.parseInt(rtmp.get(0)), Integer.parseInt(rtmp.get(1)), Integer.parseInt(rtmp.get(2)), rtmp.get(3));
-                airport.station_list.add(stat);
+                airport.longTermStationList.add(stat);
+                for (int i=0; i<airport.longTermStationList.get(0).spot_num; i++){
+                    validLongTermAnchorList.add(globalLongTermAnchorList.remove(0));
+                }
+                airport.longTermAnchorList = validLongTermAnchorList;
+                for (int i=0; i<airport.longTermAnchorList.size(); i++){
+                    airport.longTermAnchorList.get(i).setStyle("-fx-background-color: palevioletred; -fx-border-color: white");
+                }
             }
             line = reader.readLine();
         }
@@ -94,7 +169,7 @@ public class Controller implements Initializable {
             List<String> rtmp = Arrays.asList(line.split(","));
             Flight flt = new Flight(); // for each file line, create a Flight instance
             flt.setInfo(rtmp.get(0), rtmp.get(1), Integer.parseInt(rtmp.get(2)), Integer.parseInt(rtmp.get(3)), Integer.parseInt(rtmp.get(4)));
-            airport.flight_list.add(flt);
+            airport.flightList.add(flt);
             line = reader.readLine();
         }
         reader.close();
--- Airport/src/app/model/Airport.java
+++ Airport/src/app/model/Airport.java
@@ -1,6 +1,7 @@
 package app.model;
 
-import app.model.station.Station;
+import app.model.station.*;
+import javafx.scene.layout.AnchorPane;
 
 import java.util.List;
 import java.util.ArrayList;
@@ -9,7 +10,26 @@ public class Airport {
 
     double fuel = 0.25, cleaning = 0.02, transport = 0.02, loading = 0.05;
 
-    public List<Flight> flight_list = new ArrayList<Flight>();
-    public List<Station> station_list = new ArrayList<Station>();
+    public List<Flight> flightList = new ArrayList<Flight>();
 
+    public List<GateStation> gateStationList = new ArrayList<GateStation>();
+    public List<AnchorPane> gateAnchorList;
+
+    public List<TradeStation> tradeStationList = new ArrayList<TradeStation>();
+    public List<AnchorPane> tradeAnchorList;
+
+    public List<ZoneAStation> zoneAStationList = new ArrayList<ZoneAStation>();
+    public List<AnchorPane> zoneAAnchorList;
+
+    public List<ZoneBStation> zoneBStationList = new ArrayList<ZoneBStation>();
+    public List<AnchorPane> zoneBAnchorList;
+
+    public List<ZoneCStation> zoneCStationList = new ArrayList<ZoneCStation>();
+    public List<AnchorPane> zoneCAnchorList;
+
+    public List<GeneralStation> generalStationList = new ArrayList<GeneralStation>();
+    public List<AnchorPane> generalAnchorList;
+
+    public List<LongTermStation> longTermStationList = new ArrayList<LongTermStation>();
+    public List<AnchorPane> longTermAnchorList;
 }
