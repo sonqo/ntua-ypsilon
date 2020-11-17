756bb5154647bf591fcbc943e4695bf40a348e8f
Sonqo <strats_karyp@hotmail.com>
2020-11-17 09:54:24 +0200

    Completed board GUI

--- 6_MediaLab/Battleship/src/app/gui/Main.java
+++ 6_MediaLab/Battleship/src/app/gui/Main.java
@@ -4,6 +4,7 @@ import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.Parent;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.image.Image;
 import javafx.application.Application;
 
 public class Main extends Application {
@@ -13,6 +14,7 @@ public class Main extends Application {
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("MediaLab Battleship");
         primaryStage.setScene(new Scene(root, 1300, 900));
+        primaryStage.getIcons().add(new Image("file:images/battleship_v2.jpg"));
         primaryStage.show();
     }
     
