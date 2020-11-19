870e39639792d1687b1de5752dff2f47486a429c
Sonqo <strats_karyp@hotmail.com>
2020-11-19 16:12:02 +0200

    Addition on automation script

--- 7_Embedded_System_Design/1_Loop_Transformations/script.py
+++ 7_Embedded_System_Design/1_Loop_Transformations/script.py
@@ -7,7 +7,7 @@ sum_default = 0
 max_default = 0
 min_default = 10000
 for i in range(10):
-    curr = subprocess.run('./phods', stdout=PIPE)
+    curr = subprocess.run('./phods', stdout=PIPE, shell=True)
     info = int(curr.stdout.decode('utf-8').split("'")[0])
     if info < min_default:
         min_default = info
@@ -23,7 +23,7 @@ sum_opt_v1 = 0
 max_opt_v1 = 0
 min_opt_v1 = 10000
 for i in range(10):
-    curr = subprocess.run('./phods_opt_v1', stdout=PIPE)
+    curr = subprocess.run('./phods_opt_v1', stdout=PIPE, shell=True)
     info = int(curr.stdout.decode('utf-8').split("'")[0])
     if info < min_opt_v1:
         min_opt_v1 = info
@@ -37,23 +37,22 @@ print(min_opt_v1, average_opt_v1, max_opt_v1)
 acc = [1, 2, 4, 8, 16]
 subprocess.run('gcc -O0 phods_opt_v2.c -o phods_opt_v2', shell=True)
 average_opt_v2 = 10000
-# for B in acc:
-curr_sum_opt_v2 = 0
-curr_max_opt_v2 = 0
-curr_min_opt_v2 = 10000
-for i in range(10):
-    curr = subprocess.run('./phods_opt_v2 ' + str(1), stdout=PIPE, shell=True)
-    print(curr)
-    # info = int(curr.stdout.decode('utf-8').split("'")[0])
-#     if info < curr_min_opt_v2:
-#         curr_min_opt_v2 = info
-#     if info > curr_max_opt_v2:
-#         curr_max_opt_v2 = info
-#     curr_sum_opt_v2 += info
-# curr_average_opt_v2 = curr_sum_opt_v2/10
-# if curr_average_opt_v2 < average_opt_v2:
-#     best_B = B
-#     min_opt_v2 = curr_min_opt_v2
-#     max_opt_v2 = curr_max_opt_v2
-#     average_opt_v2 = curr_average_opt_v2
-# print(best_B, min_opt_v2, average_opt_v2, max_opt_v2)
+for B in acc:
+    curr_sum_opt_v2 = 0
+    curr_max_opt_v2 = 0
+    curr_min_opt_v2 = 10000
+    for i in range(10):
+        curr = subprocess.run('./phods_opt_v2 ' + str(B), stdout=PIPE, shell=True)
+        info = int(curr.stdout.decode('utf-8').split("'")[0])
+        if info < curr_min_opt_v2:
+            curr_min_opt_v2 = info
+        if info > curr_max_opt_v2:
+            curr_max_opt_v2 = info
+        curr_sum_opt_v2 += info
+    curr_average_opt_v2 = curr_sum_opt_v2/10
+    if curr_average_opt_v2 < average_opt_v2:
+        best_B = B
+        min_opt_v2 = curr_min_opt_v2
+        max_opt_v2 = curr_max_opt_v2
+        average_opt_v2 = curr_average_opt_v2
+print(best_B, min_opt_v2, average_opt_v2, max_opt_v2)
