dbf35d0fe58e8700ce714cb23058992d10bf6432
Sonqo <strats_karyp@hotmail.com>
2020-12-04 14:20:38 +0200

    Identation corrections

--- 7_Embedded_System_Design/2_Dynamic_Data_Type_Refinement/drr.h
+++ 7_Embedded_System_Design/2_Dynamic_Data_Type_Refinement/drr.h
@@ -1,19 +1,19 @@
 #define SLL_CL
-//#define DLL_CL
+// #define DLL_CL
 //#define DYN_ARR_CL
 
-//#define SLL_PK
-#define DLL_PK
+#define SLL_PK
+// #define DLL_PK
 //#define DYN_ARR_PK
 
 #if defined(SLL_CL) || defined(SLL_PK)
-#include "../synch_implementations/cdsl_queue.h"
+    #include "../synch_implementations/cdsl_queue.h"
 #endif
 #if defined(DLL_CL) || defined(DLL_PK)
-#include "../synch_implementations/cdsl_deque.h"
+    #include "../synch_implementations/cdsl_deque.h"
 #endif
 #if defined(DYN_ARR_CL) || defined(DYN_ARR_PK)
-#include "../synch_implementations/cdsl_dyn_array.h"
+    #include "../synch_implementations/cdsl_dyn_array.h"
 #endif
 
 typedef struct Packet_struct {
@@ -27,12 +27,13 @@ typedef struct Node_struct {
   	unsigned int dst_ip; //destination IP of the node
   	unsigned int no_of_packets;
 	unsigned int deficit;
-#if defined(SLL_PK)
-	cdsl_sll *pList;
-#elif defined(DLL_PK)
-	cdsl_dll *pList;
-#else
-	cdsl_dyn_array *pList;
-#endif
-} Node; 
 
+    #if defined(SLL_PK)
+        cdsl_sll *pList;
+    #elif defined(DLL_PK)
+        cdsl_dll *pList;
+    #else
+        cdsl_dyn_array *pList;
+    #endif
+
+} Node; 
